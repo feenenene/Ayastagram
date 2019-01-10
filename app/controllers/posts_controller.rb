e45bbec0911b636dbb7267ff16e8df9a7b0dee40
kajiayano <ayanyano428@gmail.com>
2019-01-10 23:21:40 +0900

    make likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i(show destroy)
 
   def index
-    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
+    @posts = Post.limit(10).includes(:photos, :user, :likes).order('created_at DESC')
   end
 
   def new
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
 
   def show
     @photos = @post.photos
+    @likes = @post.likes.includes(:user)
   end
 
   def destroy
