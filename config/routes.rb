2b207f8ac532edb387fa8665da476f9b75c2c2d3
kajiayano <ayanyano428@gmail.com>
2019-01-10 22:56:27 +0900

    make post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   devise_for :users,
     controllers: { registrations: 'registrations' }
   root 'pages#home'
   resources :users, only: %i(show)
+
+  resources :posts, only: %i(index new create) do
+    resources :photos, only: %i(create)
+  end
+
 end
